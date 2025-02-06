7cca550bc1b112c7f4c07e9343ee6169283cd60e
josh-133 <joshuaburge507@gmail.com>
2025-02-07 09:59:31 +1030

    State machine is almost there but still needs work.

--- elevator_state_machine/src/main.rs
+++ elevator_state_machine/src/main.rs
@@ -1,13 +1,22 @@
-use std::any::Any;
+use std::fmt;
 use std::io::{self, Read};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum Direction {
     Up,
     Down,
 }
 
-#[derive(Clone, Copy)]
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Direction::Down => write!(f, "Down"),
+            Direction::Up => write!(f, "Up"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
 enum State {
     Closed {
         moving: bool,
@@ -32,9 +41,73 @@ enum State {
     },
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Elevator {
     state: State
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            State::Closed { 
+                moving, 
+                direction, 
+                current_floor, 
+                dest_floor, 
+                door_open 
+            } => {
+                write!(
+                    f,
+                    "Closed (moving: {}, direction: {}, current floor: {}, destination: {}, door open: {})",
+                    moving, 
+                    direction.as_ref().map_or("None".to_string(), |d| d.to_string()), 
+                    current_floor.as_ref().map_or("None".to_string(), |c| c.to_string()), 
+                    dest_floor.as_ref().map_or("None".to_string(), |d| d.to_string()), 
+                    door_open,
+                )
+            },
+            State::Open { 
+                moving, 
+                direction, 
+                current_floor, 
+                dest_floor, 
+                door_open 
+            } => {
+                write!(
+                    f,
+                    "Open (moving: {}, direction: {}, current floor: {}, destination: {}, door open: {})",
+                    moving, 
+                    direction.as_ref().map_or("None".to_string(), |d| d.to_string()), 
+                    current_floor.as_ref().map_or("None".to_string(), |c| c.to_string()), 
+                    dest_floor.as_ref().map_or("None".to_string(), |d| d.to_string()), 
+                    door_open,
+                )
+            },
+            State::Moving { 
+                moving, 
+                direction, 
+                current_floor, 
+                dest_floor, 
+                door_open 
+            } => {
+                write!(
+                    f,
+                    "Moving (moving: {}, direction: {}, current floor: {}, destination: {}, door open: {})",
+                    moving, 
+                    direction.as_ref().map_or("None".to_string(), |d| d.to_string()), 
+                    current_floor.as_ref().map_or("None".to_string(), |c| c.to_string()), 
+                    dest_floor.as_ref().map_or("None".to_string(), |d| d.to_string()), 
+                    door_open,
+                )
+            }
+        }
+    }
+}
+
+impl fmt::Display for Elevator {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.state)
+    }
 }
 
 impl Elevator {
@@ -148,36 +221,39 @@ fn main() {
 
     let mut elevator = Elevator::new();
 
-    while true {
-        println!("Press up or down button: ");
+    println!("Press up or down button: ");
 
-        let mut select_button = String::new();
-        io::stdin().read_line(&mut select_button).expect("Invalid input");
-        let select_button = select_button.trim().to_lowercase();
+    let mut select_button = String::new();
+    io::stdin().read_line(&mut select_button).expect("Invalid input");
+    let select_button = select_button.trim().to_lowercase();
 
-        let direction: Option<Direction> = match select_button.as_str() {
-            "up" => Some(Direction::Up),
-            "down" => Some(Direction::Down),
-            _ => None,
-        };
+    let direction: Option<Direction> = match select_button.as_str() {
+        "up" => Some(Direction::Up),
+        "down" => Some(Direction::Down),
+        _ => None,
+    };
 
-        println!("Select a floor: ");
+    println!("Select a floor: ");
 
-        let mut floor: String = String::new();
-        io::stdin().read_line(&mut floor).expect("Invalid input");
-        let floor = floor.trim();
+    let mut floor: String = String::new();
+    io::stdin().read_line(&mut floor).expect("Invalid input");
+    let floor = floor.trim();
 
-        let dest_floor: Option<i8> = match floor {
-            "G" => Some(0),
-            "0" => Some(0),
-            "1" => Some(1),
-            "2" => Some(2),
-            "3" => Some(3),
-            _ => None,
-        };
+    let dest_floor: Option<i8> = match floor {
+        "G" => Some(0),
+        "0" => Some(0),
+        "1" => Some(1),
+        "2" => Some(2),
+        "3" => Some(3),
+        _ => None,
+    };
 
-        elevator.transition(dest_floor, direction);
-    }
-
-    
+    elevator.transition(dest_floor, direction);
+    print!("{}", elevator.state);
+    elevator.transition(dest_floor, direction);
+    print!("{}", elevator.state);
+    elevator.transition(dest_floor, direction);
+    print!("{}", elevator.state);
+    elevator.transition(dest_floor, direction);
+    print!("{}", elevator.state);    
 }
